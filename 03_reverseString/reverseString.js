118029f97a10060f082b75336651da40ba576f4a
Trent <trentmenard6@yahoo.com>
2023-07-12 01:35:19 -0500

    Complete exercises 1-3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strReversed = ""
+    for (let i = string.length; i >= 0; i--)
+        strReversed += string.charAt(i);
+    return strReversed;
 };
 
 // Do not edit below this line
